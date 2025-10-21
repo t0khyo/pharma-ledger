00b13a94d93389d5de2dc933d2e17bfb46ce0a03
Abdelrahman Eltokhy <abdelrahman.m.eltokhy@gmail.com>
2025-10-22 02:24:08 +0300

    feat: add hot reloading and dev script

--- src/electron/main.ts
+++ src/electron/main.ts
@@ -1,13 +1,13 @@
 import { app, BrowserWindow } from "electron";
 import path from "path";
-
-type test = string;
-
-const a: test = "Hello";
-
-console.log(a);
+import { isDev } from "./util.js";
 
 app.on("ready", () => {
-    const mainWindow = new BrowserWindow({});
-    mainWindow.loadFile(path.join(app.getAppPath(), 'dist-react/index.html'));
+  const mainWindow = new BrowserWindow({});
+
+  if (isDev()) {
+    mainWindow.loadURL("http://localhost:5123");
+  } else {
+    mainWindow.loadFile(path.join(app.getAppPath(), "dist-react/index.html"));
+  }
 });
