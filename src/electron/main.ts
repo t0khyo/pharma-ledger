3e19f6d4f128f1203a4ecabb57144851d579c138
Abdelrahman Eltokhy <abdelrahman.m.eltokhy@gmail.com>
2025-11-05 01:26:20 +0300

    feat: implement preload script and path resolver for Electron app

--- src/electron/main.ts
+++ src/electron/main.ts
@@ -3,7 +3,11 @@ import path from "path";
 import { isDev } from "./util.js";
 
 app.on("ready", () => {
-  const mainWindow = new BrowserWindow({});
+  const mainWindow = new BrowserWindow({
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+    }
+  });
 
   if (isDev()) {
     mainWindow.loadURL("http://localhost:5123");
